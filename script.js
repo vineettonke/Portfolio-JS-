3c396ab9d3dd4f3e9a15f217a0b64f4fb68fa02b
Vineet Tonke <vineettonke17@gmail.com>
2025-09-18 04:43:27 +0530

    floating cursor, hero animation

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 }); 
+
+function heroPageAnimation(){
+    var tl = gsap.timeline();
+
+    tl.from("#navbar", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+        .to(".boundingelem", {
+            y: 0,
+            ease: Expo.easeInOut,
+            duration: 1,
+            stagger: .2
+        })
+}
+
+function circleCursor(){
+    window.addEventListener("mousemove", function(dets){
+        console.log(dets);
+        document.querySelector("#circle-cursor").style.transform = `translate(${dets.clientX - 5}px, ${dets.clientY - 5}px)`
+    })
+}
+
+circleCursor();
+heroPageAnimation();
