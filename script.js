487b1bf4b598db8ff587173c315a1059ca5b32ba
Vineet Tonke <vineettonke17@gmail.com>
2025-09-22 23:53:13 +0530

    section 2: hover effect done

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Initialize Locomotive Scroll
 const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
@@ -57,18 +56,39 @@ function customCursor() {
         requestAnimationFrame(animate);
     }
     animate();
-
-    const hoverTargets = document.querySelectorAll("a, button, .hover-target");
-    hoverTargets.forEach(el => {
-        el.addEventListener("mouseenter", () => {
-            cursor.style.transform += " scale(1.5)";
-        });
-        el.addEventListener("mouseleave", () => {
-            cursor.style.transform = cursor.style.transform.replace(" scale(1.5)", "");
-        });
-    });
 }
 
-// Run animations & cursor
+document.querySelectorAll(".elem").forEach(function(elem) {
+    var rotate = 0;
+    var diffrot = 0;
+    
+    elem.addEventListener("mousemove", function(details){
+        gsap.to(elem.querySelector("img"), {
+            opacity: 0,
+            ease: Power3,
+            duration: 0.5
+        });
+    });
+});
+
+
+document.querySelectorAll(".elem").forEach(function(elem) {
+    var rotate = 0;
+    var diffrot = 0;
+    
+    elem.addEventListener("mousemove", function(details){
+        var diff = details.clientY - elem.getBoundingClientRect().top;
+        diffrot = details. clientX - rotate;
+        rotate = details.clientX;
+        gsap.to(elem.querySelector("img"), {
+            opacity: 1,
+            ease: Power3,
+            top: diff,
+            left: details.clientX,
+            rotate: gsap.utils.clamp(-20, 20, diffrot)
+        });
+    });
+});
+
 customCursor();
 heroPageAnimation();
